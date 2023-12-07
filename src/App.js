c572f95a3b956a7eedef264f637dbc374b49f8c5
Bhavu Prajapati <138212607+bvp1518@users.noreply.github.com>
2023-12-07 10:13:24 +0530

    Google place autocomplete API

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Gauto from './Gplace';
+export default function App() {
+  return (
+    <div>
+      <Gauto/>
+    </div>
+  );
+}
---
+++ src/Gplace.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+
+const Component = () => (
+    <div>
+        <GooglePlacesAutocomplete
+            apiKey="AIzaSyAx0kk4WSftebhkMYbL-ED-6nDRB0U4L6w"
+        />
+    </div>
+);
+
+export default Component;
